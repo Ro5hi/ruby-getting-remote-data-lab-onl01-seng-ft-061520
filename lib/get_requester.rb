d1ae9c30f5f4c0ce26c87e16e68e3af8f3be92df
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-03 17:35:48 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -11,9 +11,9 @@ class GetRequester
   
   def get_response_body
     uri = URI.parse(@url)
-    response = NET::HTTP.get_response(uri)
-    response.body 
-  end 
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
   
   def parse_json
     JSON.parse(get_response_body)
